71fdfd1e3420d6a3859ae0acaa4acf85abbc1f64
Linfeng <linfengleee@gmail.com>
2022-03-20 16:17:20 +0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='opentraj',
     version='1.0',
     author='Javad Amirian',
     author_email='amiryan.j@gmail.com',
-    packages=['opentraj'],
+    packages=find_packages('opentraj'),
+    package_dir={'': 'opentraj'},
     url='https://github.com/crowdbotp/OpenTraj',
     license='MIT',
     description='Tools for analyzing trajectory datasets',
